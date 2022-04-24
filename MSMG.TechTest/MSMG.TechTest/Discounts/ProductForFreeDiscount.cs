708e2c6557be86fa08fcf4ac2978b27cea3db8b5
asovde <asovde@hotmail.com>
2022-04-24 18:05:09 +0100

    Fixed test GetTotal_Returns_The_Basket_Total_Price_With_All_Discounts_Applied

--- MSMG.TechTest/MSMG.TechTest/Discounts/ProductForFreeDiscount.cs
+++ MSMG.TechTest/MSMG.TechTest/Discounts/ProductForFreeDiscount.cs
@@ -5,11 +5,12 @@
         public decimal CalculateDiscount(IList<Product> products)
         {
             var discount = 0m;
-
             var milk = products.FirstOrDefault(p => p.Name == "Milk");
 
+            var numberOfFreeMilks = (milk?.Quantity ?? 0) / 4;
+
             if (milk != null && milk.Quantity >= 4)
-                discount += milk.Price;
+                discount += milk.Price * numberOfFreeMilks;
 
             return discount;
         }
