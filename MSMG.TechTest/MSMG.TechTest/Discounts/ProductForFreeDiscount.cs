c6602e17fbd02b47da957155b7a7a1f0664a36c9
asovde <asovde@hotmail.com>
2022-04-24 18:06:07 +0100

    Refactored ProductForFreeDiscount

--- MSMG.TechTest/MSMG.TechTest/Discounts/ProductForFreeDiscount.cs
+++ MSMG.TechTest/MSMG.TechTest/Discounts/ProductForFreeDiscount.cs
@@ -4,15 +4,10 @@
     {
         public decimal CalculateDiscount(IList<Product> products)
         {
-            var discount = 0m;
             var milk = products.FirstOrDefault(p => p.Name == "Milk");
-
             var numberOfFreeMilks = (milk?.Quantity ?? 0) / 4;
 
-            if (milk != null && milk.Quantity >= 4)
-                discount += milk.Price * numberOfFreeMilks;
-
-            return discount;
+            return (milk?.Price ?? 0) * numberOfFreeMilks;
         }
     }
 }
