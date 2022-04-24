9c082d914365e18d7adfb9388514efc9cb9448a7
asovde <asovde@hotmail.com>
2022-04-24 18:16:56 +0100

    Fixed test GetTotal_Returns_The_Basket_Total_Price_With_Discount_Applied_For_Four_Butter_And_Two_Bread

--- MSMG.TechTest/MSMG.TechTest/Discounts/ProductPercentageDiscount.cs
+++ MSMG.TechTest/MSMG.TechTest/Discounts/ProductPercentageDiscount.cs
@@ -4,14 +4,23 @@
     {
         public decimal CalculateDiscount(IList<Product> products)
         {
-            var discount = 0m;
             var butterQuantity = products.Where(p => p.Name == "Butter").Select(p => p.Quantity).Sum();
+            var numberOfDiscountsToApply = butterQuantity / 2;
             var bread = products.FirstOrDefault(p => p.Name == "Bread");
 
-            if (butterQuantity >= 2)
-                discount += (bread?.Price ?? 0m) * 0.5m;
+            return Enumerable.Range(1, bread?.Quantity ?? 0)
+                .Select(p =>
+                {
+                    var discount = 0m;
 
-            return discount;
+                    if (numberOfDiscountsToApply > 0)
+                    {
+                        discount = (bread?.Price ?? 0) * 0.5m;
+                        numberOfDiscountsToApply--;
+                    }
+
+                    return discount;
+                }).Sum();
         }
     }
 }
